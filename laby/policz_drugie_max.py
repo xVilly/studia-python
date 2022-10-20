1a7ed0c3ee9d02f791a5e56aa213ed36307c8c57
Villy <mich038al@gmail.com>
2022-10-20 14:48:58 +0200

    policz drugie max

---
+++ laby/policz_drugie_max.py
@@ -0,0 +1,31 @@
+
+def policz_drugie_max():
+    ilosc_liczb = 0
+    try:
+        ilosc_liczb = int(input('Podaj ilosc liczb ktore wprowadzisz: '))
+        if ilosc_liczb < 2:
+            print('Musisz wprowadzic conajmniej jedna liczbe')
+            return
+        
+        max = float(input('Podaj liczbe: '))
+        drugie_max = max
+        c = 1
+        while c < ilosc_liczb:
+            liczba = float(input('Podaj liczbe: '))
+            if liczba > max:
+                drugie_max = max
+                max = liczba
+            elif liczba > drugie_max:
+                drugie_max = liczba
+            c += 1
+        print(f'Z wszystkich podanych liczb najwieksza byla {max}')
+        if drugie_max == max:
+            print('Drugie maximum nie istnieje')
+        else:
+            print(f'Drugim maximum jest liczba {drugie_max}')
+    except:
+        print('Wprowadzono niepoprawna liczbe')
+        return
+
+policz_drugie_max()
+
